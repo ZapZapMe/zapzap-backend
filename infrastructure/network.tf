6ff0b8a569226a8f8e82f8e8afbaf1a11944d032
Simon Tennant <simon@imaginator.com>
2024-12-19 11:55:45 +0100

    add vpc

---
+++ infrastructure/network.tf
@@ -0,0 +1,25 @@
+resource "google_compute_network" "peering_network" {
+  name                    = "private-network"
+  auto_create_subnetworks = "false"
+}
+
+resource "google_compute_global_address" "private_ip_address" {
+  name          = "private-ip-address"
+  purpose       = "VPC_PEERING"
+  address_type  = "INTERNAL"
+  prefix_length = 16
+  network       = google_compute_network.peering_network.id
+}
+
+resource "google_service_networking_connection" "default" {
+  network                 = google_compute_network.peering_network.id
+  service                 = "servicenetworking.googleapis.com"
+  reserved_peering_ranges = [google_compute_global_address.private_ip_address.name]
+}
+
+resource "google_compute_network_peering_routes_config" "peering_routes" {
+  peering              = google_service_networking_connection.default.peering
+  network              = google_compute_network.peering_network.name
+  import_custom_routes = true
+  export_custom_routes = true
+}
