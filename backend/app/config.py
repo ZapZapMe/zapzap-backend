69e79df9d0b370ee7e644765f414859d38c8f928
Simon Tennant <simon@imaginator.com>
2025-01-19 11:46:21 +0100

    update config to use Pydantic BaseSettings

--- backend/app/config.py
+++ backend/app/config.py
@@ -1,38 +1,48 @@
-import os
+from typing import Optional
 
 from dotenv import load_dotenv
+from pydantic import BaseSettings
 
 load_dotenv()
 
 
-class Settings:
-    ENVIRONMENT = os.getenv("ENVIRONMENT")  # development or production
-    GREETING = os.getenv("GREETING", "Hello, default greeting!")
-    LOCAL_DATABASE_URL = os.getenv("LOCAL_DATABASE_URL", "sqlite:///./zap-zap.sqlite3")
+class Settings(BaseSettings):
+    ENVIRONMENT: str = "production"  # development or production
+    GREETING: str = "Hello, default greeting!"
+    LOCAL_DATABASE_URL: str = "sqlite:///./zap-zap.sqlite3"
 
-    TWITTER_OAUTH2_CLIENT_ID = os.environ.get("TWITTER_OAUTH2_CLIENT_ID")
-    TWITTER_OAUTH2_CLIENT_SECRET = os.environ.get("TWITTER_OAUTH2_CLIENT_SECRET")
-    TWITTER_REDIRECT_URI = os.environ.get("TWITTER_REDIRECT_URI")
-    TWITTER_ACCOUNT_BEARER_TOKEN = os.environ.get("TWITTER_ACCOUNT_BEARER_TOKEN")
-    TWITTER_ACCESS_TOKEN = os.environ.get("TWITTER_ACCESS_TOKEN")
-    TWITTER_ACCESS_TOKEN_SECRET = os.environ.get("TWITTER_ACCESS_TOKEN_SECRET")
+    # Required settings (no default value)
+    TWITTER_OAUTH2_CLIENT_ID: str
+    TWITTER_OAUTH2_CLIENT_SECRET: str
+    TWITTER_REDIRECT_URI: str
+    TWITTER_ACCOUNT_BEARER_TOKEN: str
+    TWITTER_ACCESS_TOKEN: str
+    TWITTER_ACCESS_TOKEN_SECRET: str
 
-    JWT_SECRET_KEY = os.environ.get("JWT_SECRET_KEY")
-    JWT_ALGORITHM = os.environ.get("JWT_ALGORITHM", "HS256")
-    JWT_ACCESS_TOKEN_EXPIRE_SECONDS = int(os.environ.get("JWT_ACCESS_TOKEN_EXPIRE_SECONDS", 3600))
+    JWT_SECRET_KEY: str
+    JWT_ALGORITHM: str = "HS256"
+    JWT_ACCESS_TOKEN_EXPIRE_SECONDS: int = 3600
 
-    BREEZ_API_KEY = os.environ.get("BREEZ_API_KEY")
-    BREEZ_MNEMONIC = os.environ.get("BREEZ_MNEMONIC")
-    BREEZ_WORKING_DIR = os.environ.get("BREEZ_WORKING_DIR", "./")
-    BREEZ_GREENLIGHT_INVITE = os.environ.get("BREEZ_GREENLIGHT_INVITE")
+    BREEZ_API_KEY: str
+    BREEZ_MNEMONIC: str
+    BREEZ_WORKING_DIR: str
+    BREEZ_GREENLIGHT_INVITE: str
 
-    # only valid on production
-    DB_INSTANCE_CONNECTION_NAME = os.getenv("DB_INSTANCE_CONNECTION_NAME")
-    DB_USER = os.getenv("DB_USER")
-    DB_PASS = os.getenv("DB_PASS")
-    DB_NAME = os.getenv("DB_NAME")
-    GREENLIGHT_CLIENT_CERTIFICATE = os.getenv("GREENLIGHT_CLIENT_CERTIFICATE")
-    GREENLIGHT_CLIENT_PRIVATE_KEY = os.getenv("GREENLIGHT_CLIENT_PRIVATE_KEY")
+    # only valid on production so "Optional"
+    DB_INSTANCE_CONNECTION_NAME: Optional[str] = None
+    DB_USER: Optional[str] = None
+    DB_PASS: Optional[str] = None
+    DB_NAME: Optional[str] = None
+
+    GREENLIGHT_CLIENT_CERTIFICATE: str
+    GREENLIGHT_CLIENT_PRIVATE_KEY: str
+
+    # Application settings
+    LEADERBOARD_CALCULATION_WINDOW_DAYS: int = 30  # a value in .env would override this
+    TWITTER_AVATAR_CACHE_TTL_DAYS: int = 30
+
+    class Config:
+        env_file = ".env"
 
 
 settings = Settings()
