edc9d2f2fa531c4df7d9c46cd3e017634bd61e1e
Simon Tennant <simon@imaginator.com>
2025-01-20 07:55:10 +0100

    add "tweet.write" scope

--- backend/app/utils/twitter_oauth.py
+++ backend/app/utils/twitter_oauth.py
@@ -7,7 +7,7 @@ AUTHORIZE_URL = "https://twitter.com/i/oauth2/authorize"
 TOKEN_URL = "https://api.twitter.com/2/oauth2/token"
 USERINFO_URL = "https://api.twitter.com/2/users/me"
 
-SCOPES = ["tweet.read", "users.read"]
+SCOPES = ["tweet.read", "tweet.write", "users.read"]
 
 
 def get_authorization_url():
