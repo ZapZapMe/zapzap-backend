5ac640c382dabda503a3115ff364e87a8ace4160
Simon Tennant <simon@imaginator.com>
2025-02-14 14:04:53 +0100

    fix: show tips on profile even if not paid out

--- backend/app/routes/tips.py
+++ backend/app/routes/tips.py
@@ -112,7 +112,7 @@ def create_tip(
         username, tweet_id = extract_username_and_tweet_id(tip_data.tweet_url)
         # Convert username to lowercase immediately
         username = username.lower()
-        
+
         tweet = db.query(Tweet).filter(Tweet.id == tweet_id).first()
         if not tweet:
             logging.info(f"Tweet {tweet_id} not found. Creating new tweet.")
@@ -171,6 +171,7 @@ def create_tip(
         logging.error(f"Unexpected error occurred: {e}")
         raise HTTPException(status_code=400, detail=f"Failed to create tip. Reason: {str(e)}")
 
+
 @router.get("/", response_model=list[TipOut])
 def list_tips(db: Session = Depends(get_db)):
     # Order tips from new to old
@@ -204,7 +205,7 @@ def get_sent_tips_by_username(username: str, db: Session = Depends(get_db)):
         .join(User, User.id == Tip.tip_sender)
         .join(Tweet, Tweet.id == Tip.tweet_id)
         .join(User2, User2.id == Tweet.tweet_author)
-        .filter(Tip.tip_sender == user.id, Tip.paid_out.is_(True))
+        .filter(Tip.tip_sender == user.id)
         .order_by(Tip.created_at.desc())  # Order from new to old
         .all()
     )
@@ -232,7 +233,7 @@ def get_received_tips_by_username(username: str, db: Session = Depends(get_db)):
     user = db.query(User).filter(func.lower(User.twitter_username) == username.lower()).first()
     if not user:
         raise HTTPException(status_code=400, detail="User not found.")
-    
+
     tips = (
         db.query(Tip)
         .join(Tweet, Tweet.id == Tip.tweet_id)
@@ -244,7 +245,7 @@ def get_received_tips_by_username(username: str, db: Session = Depends(get_db)):
 
     sender_usernames = [tip.sender.twitter_username for tip in tips if tip.sender is not None]
     avatars_map = get_avatars_for_usernames(sender_usernames, db)
-    
+
     return [
         TipSummary(
             tip_sender=tip.sender.twitter_username if tip.sender else None,
